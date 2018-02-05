3ee5e9ac364f7de4216e99541a1d91164784e13b
Srinidhi Prabandham <srinidhi.prabandham@icicletech.com>
2018-02-05 17:24:03 +0530

    bug fix for user_session timeout configuration

--- lib/demo_heroku_deployment_web/channels/user_socket.ex
+++ lib/demo_heroku_deployment_web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule DemoHerokuDeploymentWeb.UserSocket do
   # channel "room:*", DemoHerokuDeploymentWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
     timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
